5ebcc546abf6ea5f3e9a0da943d8073b891c5ea5
dcask <dcask@mail.ru>
2024-01-16 23:42:10 +0300

    go

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,10 @@ RUN mkdir -p /app
 COPY ./app /app
 
 WORKDIR /app
+COPY requirements.txt /app/requirements.txt
 
 RUN pip install --upgrade pip
 RUN pip uninstall jwt
-RUN pip install -r requirements.txt
+RUN pip install -r /app/requirements.txt
 
 CMD ["uvicorn", "main:app", "--host","0.0.0.0", "--port", "80"]
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #проверять и удалять старый образ
-old_vicontrol=$(docker image ls | grep vicintrol)
-if [[!-z $old_vicontrol]]; then
-docker image rm vicontrol
+old_vicontrol=$(docker image ls | grep vicontrol)
+
+if [ -n  "$old_vicontrol" ]; then
+   docker image rm vicontrol --froce
 fi
+
 docker build -t vicontrol . < Dockerfile
