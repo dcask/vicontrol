910f703f6e3bd28123c1ecc8d785a1dcad379e88
dcask <dcask@mail.ru>
2024-01-29 17:41:47 +0300

    ws

--- wsjs.js
+++ wsjs.js
@@ -1,5 +1,3 @@
-//let f_inj='<script>function PopUpShow(){$("#popup1").show();} function PopUpHide(){$("#popup1").hide();}</script>';
-//let inj='<div class="b-popup" id="popup1"><div class="b-popup-content"><span id="popupcontent"></span></br><a href="javascript:PopUpHide()">Close</a></div></div>';
 let f_inj='<script>function PopUpShow(){$("#admmessage-popup1").css({"visibility":"visible","opacity":1});}function PopUpHide(){$("#admmessage-popup1").css({"visibility":"hidden","opacity":0});}</script>';
 let inj=`<div id="admmessage-popup1" class="admmessage-overlay">
 <div class="admmessage-popup">
@@ -10,29 +8,48 @@ let inj=`<div id="admmessage-popup1" class="admmessage-overlay">
 </div>
 </div>
 `
-let token=JSON.parse(sessionStorage['oidc.user:/idsrv:DashboardsApp'])["access_token"]
-const urlParams = new URLSearchParams(window.location.search);
-const dashboardguid = urlParams.get('dashboardGuid')
-let command={"command":"auth","token":token,"dashboard":dashboardguid};
-$( document ).ready(function() {
-$("body").append(f_inj);
-$("body").append(inj);
-});
-//$(document).ready(function(){ PopUpHide();});
-let getUrl = window.location;
-let baseUrl = getUrl.host; //"/" + getUrl.pathname.split('/')[1];
-let client_id = Date.now()
-const visSocket = new WebSocket('wss://'+baseUrl+"/control/ws/"+client_id);
-visSocket.onopen = () => visSocket.send(JSON.stringify(command));
-console.log('wss connected');
-visSocket.onmessage = (event) => {
-    console.log(event.data);
-    let c=JSON.parse(event.data);
+let timerId = setInterval(()=>{
+    const urlParams = new URLSearchParams(window.location.search);
+    const dashboardguid = urlParams.get('dashboardGuid');
+    if (dashboardguid !=null){
+        clearInterval(timerId);
+	$("body").append(f_inj);
+	$("body").append(inj);
+	const getUrl = window.location;
+	const baseUrl = getUrl.host; //"/" + getUrl.pathname.split('/')[1];
     
-    if(c.command=='message')
-    {
-       $("#admmessage-content").text(c.text);
-       PopUpShow();
+	const dd_flag=urlParams.get('isEditing');
+    
+	let token='';
+	try {
+	    if (dd_flag==='true')
+    	    {
+		key=`oidc.user:${getUrl.protocol}//${getUrl.host}/idsrv:dashboard_viewer`;
+		token=JSON.parse(sessionStorage[key])["access_token"]
+	    }
+    	    else
+	    {
+		token=JSON.parse(sessionStorage['oidc.user:/idsrv:DashboardsApp'])["access_token"]
+	    }
+	}
+	catch(e)
+	{
+    	console.log(e);
+	}
+	const command={"command":"auth","token":token,"dashboard":dashboardguid,"dd":dd_flag};
+	const client_id = Date.now()
+	const visSocket = new WebSocket('wss://'+baseUrl+"/control/ws/"+client_id);
+	visSocket.onopen = () => visSocket.send(JSON.stringify(command));
+	console.log('wss connected');
+	visSocket.onmessage = (event) => {
+	    const  c=JSON.parse(event.data);
+    	    if(c.command=='message')
+	    {
+    		$("#admmessage-content").text(c.text);
+    		PopUpShow();
+	    }
+	}
     }
-}
+}, 5000);
+
 
